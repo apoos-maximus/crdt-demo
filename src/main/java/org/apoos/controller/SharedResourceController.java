c0f1f5b3d5bcbab65950dad89249c0a3b316a29c
Apoorv Sachan <apoorvsachan@fico.com>
2020-10-07 12:24:06 +0530

    update project

---
+++ src/main/java/org/apoos/controller/SharedResourceController.java
@@ -0,0 +1,9 @@
+package org.apoos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SharedResourceController {
+
+}
---
+++ src/main/java/org/apoos/pojo/SharedResource.java
@@ -0,0 +1,17 @@
+package org.apoos.pojo;
+
+public class SharedResource {
+    private String paragraph;
+
+    public SharedResource(String value) {
+        this.paragraph = value;
+    }
+
+    public String getParagraph() {
+        return paragraph;
+    }
+
+    public void setParagraph(String paragraph) {
+        this.paragraph = paragraph;
+    }
+}
