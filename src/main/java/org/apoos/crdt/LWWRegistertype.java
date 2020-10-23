011e981d4d3c41145fdc5bdd1489bc8809676e05
Apoorv Sachan <apoorvsachan@fico.com>
2020-10-23 23:46:18 +0530

    LWW Register

---
+++ src/main/java/org/apoos/crdt/LWWRegistertype.java
@@ -0,0 +1,41 @@
+package org.apoos.crdt;
+
+import com.netopyr.wurmloch.crdt.LWWRegister;
+import com.netopyr.wurmloch.store.LocalCrdtStore;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class LWWRegistertype {
+    String type = "Last Writer Wins";
+
+    public static void merge(){
+        LocalCrdtStore crdtStore1 = new LocalCrdtStore("N_1");
+        LocalCrdtStore crdtStore2 = new LocalCrdtStore("N_2");
+        crdtStore1.connect(crdtStore2);
+
+        LWWRegister<String> replica1 = crdtStore1.createLWWRegister("ID_1");
+        LWWRegister<String> replica2 = crdtStore2.<String>findLWWRegister("ID_1").get();
+
+        replica1.set("apple");
+        replica2.set("banana");
+
+        assertThat(replica1.get(), is("banana"));
+        assertThat(replica2.get(), is(("banana")));
+
+        //disconnect
+
+        crdtStore1.disconnect(crdtStore2);
+
+        replica1.set("strawberry");
+        replica2.set("pear");
+
+        assertThat(replica1.get(), is("strawberry"));
+        assertThat(replica2.get(), is("pear"));
+
+        crdtStore1.connect(crdtStore2);
+
+        assertThat(replica1.get(), is("pear"));
+        assertThat(replica2.get(), is("pear"));
+    }
+}
---
+++ src/main/java/org/apoos/main/runLWW.java
@@ -0,0 +1,9 @@
+package org.apoos.main;
+
+import org.apoos.crdt.LWWRegistertype;
+
+public class runLWW {
+    public static void main(String[] args) {
+        LWWRegistertype.merge();
+    }
+}
