0e0dcb9eaa7df61bb99bc2612eeaadc7aab1e6e9
Apoorv Sachan <apoorvsachan@fico.com>
2020-11-03 15:31:14 +0530

    demo complete

--- src/main/java/org/apoos/crdt/RGAType.java
+++ src/main/java/org/apoos/crdt/RGAType.java
@@ -3,6 +3,9 @@ package org.apoos.crdt;
 import com.netopyr.wurmloch.crdt.RGA;
 import com.netopyr.wurmloch.store.LocalCrdtStore;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
@@ -54,5 +57,29 @@ public class RGAType {
         System.out.println(replica1.toString());
         System.out.println(replica2.toString());
         System.out.println("=====================================");
+    }
+
+    public static void merge_char(){
+        final LocalCrdtStore crdtStore1 = new LocalCrdtStore();
+        final LocalCrdtStore crdtStore2 = new LocalCrdtStore();
+        crdtStore1.connect(crdtStore2);
+
+        final RGA<Character> replica1 = crdtStore1.createRGA("ID_1");
+        final RGA<Character> replica2 = crdtStore2.<Character>findRGA("ID_1").get();
+
+        String delta = "homelander";
+        int pos = 0;
+        for (int i = 0; i < delta.length() ; i++) {
+            replica1.add(delta.charAt(i));
+        }
+        delta = " goes to school !";
+        pos = replica2.size();
+        for (int i = 0; i < delta.length() ; i++) {
+            replica2.add(pos+i, delta.charAt(i));
+        }
+        for (Character q: replica1
+             ) {
+            System.out.print(q);
+        }
     }
 }
---
+++ src/main/java/org/apoos/main/RGACharMerge.java
@@ -0,0 +1,9 @@
+package org.apoos.main;
+
+import org.apoos.crdt.RGAType;
+
+public class RGACharMerge {
+    public static void main(String[] args) {
+        RGAType.merge_char();
+    }
+}
